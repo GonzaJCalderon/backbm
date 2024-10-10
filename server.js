414349ee1e905cde09ec8a29f24a3312e6903aab
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2024-10-10 12:45:55 -0300

    modifcacion server

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const { Sequelize } = require('sequelize');
-
-// Carga las variables de entorno
 require('dotenv').config();
 
-// Configuración de Sequelize
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
 });
 
-// Verifica la conexión
 sequelize.authenticate()
   .then(() => {
     console.log('Conexión a la base de datos exitosa');
