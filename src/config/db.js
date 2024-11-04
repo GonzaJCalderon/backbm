051d687bace4d66e66f1cb008238370a2b7edc83
Cristian <cristianchiera@gmail.com>
2024-11-04 12:54:28 -0300

    nuebas rutas compra bien

--- src/config/db.js
+++ src/config/db.js
@@ -5,24 +5,12 @@ require('dotenv').config();
 
 // Verifica que las variables de entorno están cargadas
 console.log('Database config:', {
-<<<<<<< HEAD
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT
 });
-=======
-  DB_NAME: process.env.DB_BM,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  DB_HOST: process.env.DB_HOSTNAME,
-  DB_PORT: process.env.DB_PORT
-});
-const { DB_BM, DB_USER, DB_PASS, DB_HOSTNAME, DB_PORT } = process.env
-
-
->>>>>>> develop
 
 // Configuración de Sequelize
 // Configuración de Sequelize
@@ -30,11 +18,7 @@ const sequelize = new Sequelize('bienes_muebles', 'cchiera', 'Chiera+3', {
   host: '10.100.1.80',
   port: '5432',
   dialect: 'postgres',
-<<<<<<< HEAD
   logging: false, // Cambia a true si necesitas ver las consultas SQL
-=======
-  logging: console.log, // Cambia a true si necesitas ver las consultas SQL
->>>>>>> develop
 });
 
 // Verifica la conexión
@@ -47,8 +31,3 @@ sequelize.authenticate()
   });
 
 module.exports = sequelize;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> develop
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuariosController');
-<<<<<<< HEAD
 const { verifyToken, verificarPermisos } = require('../middlewares/authMiddleware');
-=======
-const verifyToken = require('../middlewares/authMiddleware');
->>>>>>> develop
 
 // Ruta para registrar un usuario
 router.post('/register', usuarioController.crearUsuario);
@@ -13,24 +9,15 @@ router.post('/register', usuarioController.crearUsuario);
 // Ruta para login
 router.post('/login', usuarioController.loginUsuario);
 
-<<<<<<< HEAD
 // Ruta para obtener todos los usuarios (protegida y solo accesible para administrador o moderador)
 router.get('/', verifyToken, verificarPermisos(['administrador', 'moderador', ]), usuarioController.obtenerUsuarios);
 
 // Ruta para obtener un usuario por DNI (protegida y accesible solo para administrador)
 router.get('/dni', verifyToken, verificarPermisos(['administrador', 'moderador']), usuarioController.obtenerUsuarioPorDni);
-=======
-// Ruta para obtener todos los usuarios (protegida)
-router.get('/', verifyToken, usuarioController.obtenerUsuarios);
-
-// Ruta para obtener un usuario por DNI (protegida)
-router.get('/dni', verifyToken, usuarioController.obtenerUsuarioPorDni);
->>>>>>> develop
 
 // Ruta para registrar usuario por tercero
 router.post('/register-usuario-por-tercero', usuarioController.registerUsuarioPorTercero);
 
-<<<<<<< HEAD
 // Ruta para obtener los detalles del usuario autenticado (protegida, accesible para administrador y moderador)
 router.get('/usuario/detalles', verifyToken, verificarPermisos(['administrador', 'moderador']), usuarioController.obtenerUsuarioDetalles);
 
@@ -84,60 +71,3 @@ router.put('/:id/cambiar-rol', verifyToken, verificarPermisos(['administrador'])
 
 module.exports = router;
  
-=======
-// Ruta para obtener los detalles del usuario autenticado (requiere autenticación)
-router.get('/usuario/detalles', verifyToken, usuarioController.obtenerUsuarioDetalles);
-
-// Ruta para obtener todos los compradores (protegida)
-router.get('/compradores', verifyToken, usuarioController.obtenerCompradores);
-
-// Ruta para obtener usuarios pendientes (protegida)
-
-router.get('/usuarios/pendientes', verifyToken, usuarioController.obtenerUsuariosPendientes);
-
-
-// Ruta para obtener un usuario por su ID (protegida)
-router.get('/:id', verifyToken, usuarioController.obtenerUsuarioPorId);
-
-// Ruta para actualizar un usuario por su ID (protegida)
-router.put('/:id', verifyToken, usuarioController.actualizarUsuario);
-
-// Ruta para obtener detalles del usuario por su ID (protegida)
-router.get('/:id/detalles', verifyToken, usuarioController.obtenerUsuarioDetalles);
-
-// Ruta para eliminar un usuario por su ID (protegida)
-router.delete('/:id', verifyToken, usuarioController.eliminarUsuario);
-
-// Ruta para asignar un rol temporal (protegida)
-router.put('/:id/rolTemporal', verifyToken, usuarioController.asignarRolTemporal);
-
-// Ruta para obtener el rol temporal de un usuario (protegida)
-router.get('/:id/rolTemporal', verifyToken, usuarioController.obtenerRolTemporal);
-
-// Ruta para remover el rol temporal de un usuario (protegida)
-router.delete('/:id/rolTemporal', verifyToken, usuarioController.removerRolTemporal);
-
-// Ruta para obtener compras y ventas asociadas a un usuario (protegida)
-router.get('/:id/compras-ventas', verifyToken, usuarioController.obtenerComprasVentasPorUsuario);
-
-// Ruta para obtener todas las compras y ventas (protegida)
-router.get('/compras-ventas', verifyToken, usuarioController.obtenerComprasVentas);
-
-// Ruta para aprobar un usuario (protegida)
-router.put('/:id/aprobar', verifyToken, usuarioController.aprobarUsuario);
-
-// Ruta para rechazar un usuario (protegida)
-router.put('/:id/rechazar', verifyToken, usuarioController.rechazarUsuario);
-
-
-// Ruta para obtener todos los usuarios aprobados (protegida)
-router.get('/usuarios/aprobados', verifyToken, usuarioController.obtenerUsuariosAprobados);
-
-// Ruta para obtener todos los usuarios aprobados (protegida)
-router.get('/usuarios/rechazados', verifyToken, usuarioController.obtenerUsuariosRechazados);
-
-// Ruta para cambiar el rol del usuario a 'administrador'
-router.put('/:id/cambiar-rol', verifyToken, usuarioController.cambiarRol);
-
-module.exports = router;
->>>>>>> develop
